89e691fb28e28eeee8097c650a663973ec914b6c
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-01-14 17:52:45 -0500

    Add blank readme and placeholder app.js file

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+//multer
+//cloudinary
+//streamifier
+
+//expressSession
+//passport
+//jwt
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}!`);
+  console.log(`Listening on port ${PORT}.`);
 });
