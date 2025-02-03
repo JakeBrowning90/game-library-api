fc4b240e7468f1804de19ba886c42d6d1fe348d6
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-02-03 16:31:19 -0500

    Update prisma client

--- middleware/validateTag.js
+++ middleware/validateTag.js
@@ -3,20 +3,20 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const validateTag = [
-    body("tagName")
+  body("tagName")
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("Tag name must contain between 1 and 30 characters.")
     .custom(async (value) => {
-        const existingTagName = await prisma.tag.findUnique({
-          where: {
-            tagName: value,
-          },
-        });
-        if (existingTagName) {
-          throw new Error("Tag name already in use.");
-        }
-      }),
+      const existingTagName = await prisma.tag.findUnique({
+        where: {
+          tagName: value,
+        },
+      });
+      if (existingTagName) {
+        throw new Error("Tag name already in use.");
+      }
+    }),
 ];
 
 module.exports = validateTag;
