47d44fcc1d3e877bb0777a2142bc93349518676f
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-02-15 21:17:54 -0500

    Correct run file in docker

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("*", (req, res, next) => {
   res.send("Not a route.");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "::", () => {
   console.log(`Listening on port ${PORT}.`);
 });
